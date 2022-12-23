5aed5dc390fcba42bb56c4b53048341f82f29d69
aslnbxrz <bexruz.aslonov1@gmail.com>
2022-12-23 06:08:42 +0500

    migration publish configured

--- src/FileManagerServiceProvider.php
+++ src/FileManagerServiceProvider.php
@@ -4,18 +4,55 @@ namespace Aslnbxrz\FileManager;
 
 use Aslnbxrz\FileManager\Http\Repository\FileRepository;
 use Aslnbxrz\FileManager\Http\Repository\Interfaces\FileInterface;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 
 class FileManagerServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-//        $this->loadRoutesFrom(__DIR__.'/Routes/api.php');
+        $this->offerPublishing();
     }
 
     public function register()
     {
         $this->app->bind(FileInterface::class, FileRepository::class);
         $this->app->register(RouteServiceProvider::class);
+    }
+
+    protected function offerPublishing()
+    {
+        if (!function_exists('config_path')) {
+            // function not available and 'publish' not relevant in Lumen
+            return;
+        }
+
+        $this->publishes([
+            __DIR__ . '/Database/Migrations/create_files_table.php.stub' => $this->getMigrationFileName('create_files_table.php'),
+            __DIR__ . '/Database/Migrations/create_folders_table.php.stub' => $this->getMigrationFileName('create_folders_table.php'),
+        ], 'migrations');
+    }
+
+    /**
+     * Returns existing migration file if found, else uses the current timestamp.
+     *
+     * @param $migrationFileName
+     * @return string
+     * @throws BindingResolutionException
+     */
+    protected function getMigrationFileName($migrationFileName): string
+    {
+        $timestamp = date('Y_m_d_His');
+
+        $filesystem = $this->app->make(Filesystem::class);
+
+        return Collection::make($this->app->databasePath() . DIRECTORY_SEPARATOR . 'migrations' . DIRECTORY_SEPARATOR)
+            ->flatMap(function ($path) use ($filesystem, $migrationFileName) {
+                return $filesystem->glob($path . '*_' . $migrationFileName);
+            })
+            ->push($this->app->databasePath() . "/migrations/{$timestamp}_{$migrationFileName}")
+            ->first();
     }
 }
